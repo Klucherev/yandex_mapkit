b842ed21abf0eb7566c95b2c42f560ac57d20391
Alexey Klyucherev <klucherev@bizapp.ru>
2021-11-02 21:00:40 +0800

    fix-1.1.1-placemark

--- ios/Classes/YandexMapController.swift
+++ ios/Classes/YandexMapController.swift
@@ -700,7 +700,7 @@ public class YandexMapController: NSObject, FlutterPlatformView {
       ]
       methodChannel.invokeMethod("onMapObjectTap", arguments: arguments)
 
-      return false
+      return true
     }
   }
 
